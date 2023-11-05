30cb4b5e04c1bf16c37c73fb05ccdf1610cdb74d
kamalkassimi <othmankassimi07@gmail.com>
2023-11-05 21:41:54 +0000

    modified in file kotlin

---
+++ solution/solution_Array.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun getWinner(arr: IntArray, k: Int): Int {
+        var maxElement:Int = arr.maxOrNull()?:0
+        var  curr= arr[0] 
+        var  winstreak:Int = 0  
+       for(i in 0..arr.size){
+           var   opponent= arr[i]
+           if( curr > opponent){
+                    winstreak += 1
+           }        
+            else{
+                curr = opponent
+                winstreak = 1
+            }
+               
+            if( winstreak == k || curr == maxElement ){
+                         return curr 
+            }
+                   
+        }
+        return -1 
+    }
+}
